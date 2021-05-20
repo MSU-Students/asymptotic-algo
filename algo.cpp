1b20dcf01d0b2244270d0aeae5eadfbc57aa14d2
fencim <fencim@gmail.com>
2021-05-20 17:58:39 +0800

    logarithmic time

--- algo.cpp
+++ algo.cpp
@@ -26,6 +26,17 @@ int sum(int n) {
 }
 //sum(n) => f(n) => O(1)  + O(1) + n x (O(1)) => O(1) + O(n) => O(n)
 
+//3) O(log(n)) or O(log_2(n))
+int sumLog(int n) {
+    int nTotal = 0;//O(1)
+    for (int i = 1; i <= n; i = i * 2) { //* 2 -> is the base of log
+        nTotal += i;//O(1)
+    }
+    return n;//O(1
+}
+//sumLog(n) => f(n) => O(1)  + O(log _2(n)) => O(log _2(n))
+
+
 int main() {   
     {
         ofstream fout("constant_perf.csv");
@@ -47,5 +58,17 @@ int main() {
             fout << n << ", "<< timer.elapsed() << endl;
         }
     }
+
+    {
+        ofstream fout("logarithmic_perf.csv");
+        fout << "n, lapse" << endl;
+        Timer timer;
+        for (int n = 1; n < 1000; n++) {
+            timer.reset();
+            cout  << sumLog(n) << endl;
+            fout << n << ", "<< timer.elapsed() << endl;
+        }
+    }
+    
     return 0;
 }
