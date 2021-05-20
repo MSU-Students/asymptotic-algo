124183541d7dbad49df2c2900cb9556f3549de80
fencim <fencim@gmail.com>
2021-05-20 17:49:06 +0800

    Constant Asymtotic Algo

--- algo.cpp
+++ algo.cpp
@@ -10,9 +10,22 @@ using namespace std;
 //3) Use as bases programming
 //4) Effectiveness
 
+//1) O(1) ~ O(1 + 1) - Constatant
+void hello(int n) {
+    cout << "Hello World" << endl;//O(1)   
+}
+//hello(n) => f(n) => O(1) 
 
 int main() {   
-   
-
+    {
+        ofstream fout("constant_perf.csv");
+        fout << "n, lapse" << endl;
+        Timer timer;
+        for (int n = 1; n < 35; n++) {
+            timer.reset();
+            hello(n);
+            fout << n << ", "<< timer.elapsed() << endl;
+        }
+    }
     return 0;
 }
