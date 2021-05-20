47edff978e2a017dd5e1d6a23f57aedae7394bd2
fencim <fencim@gmail.com>
2021-05-20 17:45:42 +0800

    initial commit

---
+++ algo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include "timer.h"
+
+using namespace std;
+//What are things to consider in designing algorigthms?
+//1) RunTime
+//2) Space / Memory
+
+//3) Use as bases programming
+//4) Effectiveness
+
+
+int main() {   
+   
+
+    return 0;
+}
