f906ae58bc4db72a9bd24b7db65c4031316c47d0
fencim <fencim@gmail.com>
2021-05-20 17:55:12 +0800

    Linear Time

--- algo.cpp
+++ algo.cpp
@@ -16,14 +16,34 @@ void hello(int n) {
 }
 //hello(n) => f(n) => O(1) 
 
+//2) O(n) - Linear 
+int sum(int n) {
+    int nTotal = 0; //O(1)
+    for (int i = 1; i <= n; i++) {
+        nTotal += i;
+    }
+    return nTotal;
+}
+//sum(n) => f(n) => O(1)  + O(1) + n x (O(1)) => O(1) + O(n) => O(n)
+
 int main() {   
     {
         ofstream fout("constant_perf.csv");
         fout << "n, lapse" << endl;
         Timer timer;
-        for (int n = 1; n < 35; n++) {
+        for (int n = 1; n < 500; n++) {
             timer.reset();
             hello(n);
+            fout << n << ", "<< timer.elapsed() << endl;
+        }
+    }
+    {
+        ofstream fout("linear_perf.csv");
+        fout << "n, lapse" << endl;
+        Timer timer;
+        for (int n = 1; n < 1000; n++) {
+            timer.reset();
+            cout  << sum(n) << endl;
             fout << n << ", "<< timer.elapsed() << endl;
         }
     }
